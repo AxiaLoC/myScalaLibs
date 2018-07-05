2bf2a8d402fbe4a47893bd21b8723e651e1a6702
AxiaLoC <c.rossi86@yahoo.it>
2018-07-05 11:13:43 +0200

    log

--- PopuliUtil/loggerManager.scala
+++ PopuliUtil/loggerManager.scala
@@ -1,11 +1,18 @@
 package myScalaLibs.PopuliUtil
 
+import java.time.format.DateTimeFormatter
 import java.time.{LocalDateTime, ZoneOffset}
 
 object loggerManager  {
   var gestoreFile = new gestoreInOut ("logger2.txt")
-  def writeLine(myLine: String, append: Boolean = true): Unit = gestoreFile.writeOnFile(
-    "[" + LocalDateTime.now(ZoneOffset.UTC).toString+
-      " "+LocalDateTime.now().getHour+":"+LocalDateTime.now().getMinute+":"+LocalDateTime.now().getSecond+"]  "+
-      myLine,append)
+  def writeLine(myLine: String, append: Boolean = true): Unit = {
+
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+    val formatter2 = DateTimeFormatter.ofPattern("HH:mm:ss")
+
+    gestoreFile.writeOnFile(
+      "[" + LocalDateTime.now(ZoneOffset.UTC).format(formatter)+
+        " "+LocalDateTime.now().format(formatter2)+"]  "+
+        myLine,append)
+  }
 }
